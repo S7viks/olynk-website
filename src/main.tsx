990a7a354be9d4ff2ead5e7cdc334e3a46ab14fe
Your Name <sathvik.chenna@gmail.com>
2025-08-06 17:09:56 +0530

    Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -13,6 +15,8 @@ try {
   createRoot(rootElement).render(
     <StrictMode>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </StrictMode>
   );
 } catch (error) {
